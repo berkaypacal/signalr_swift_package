8d0e732d33b00c068ab6ddba164e5d724a05caf2
berkaypacal <pacalberkay@gmail.com>
2022-12-19 11:49:36 +0300

    update

--- Sources/signalr_swift/signalr_swift.swift
+++ Sources/signalr_swift/signalr_swift.swift
@@ -3,19 +3,20 @@ import SignalRClient
 
 public class SignalRService {
     private var connection: HubConnection
+    private var connectionUrl: URL? = URL(string: "https://192.168.1.3:7007/Chat")
     
-    public init(url: URL) {
-        connection = HubConnectionBuilder(url: url).withLogging(minLogLevel: .error).build()
-        connection.on(method: "MessageReceived", callback: { (user: String, message: String) in
+    public init() {
+        connection = HubConnectionBuilder(url: connectionUrl!).withLogging(minLogLevel: .error).build()
+        connection.on(method: "BroadcastMessage", callback: { (message: String) in
             do {
-                self.handleMessage(message, from: user)
+                self.handleMessage(message)
             }
         })
         
         connection.start()
     }
     
-    private func handleMessage(_ message: String, from user: String) {
+    private func handleMessage(_ message: String) {
         print(message)
     }
 }
