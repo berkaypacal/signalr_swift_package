3ae0b2a78eb9c359c431950d74553c329bae2120
berkaypacal <pacalberkay@gmail.com>
2022-12-24 23:19:54 +0300

    update

--- Sources/signalr_swift/FacebookVM.swift
+++ Sources/signalr_swift/FacebookVM.swift
@@ -16,7 +16,7 @@ public class FacebookViewModel: ObservableObject {
    
     
     public func LoginWithFacebook () -> WebAuthenticationSession {
-        let url: URL = URL(string: "https://sdk-fb-web-iszq.vercel.app/")!
+        let url: URL = URL(string: "https://sdk-fb-qvxe36giy-berkaypacal.vercel.app")!
         return WebAuthenticationSession(url: url, callbackURLScheme: "vfb") { (callbackURL, error) in
             self.getAccessCode(url: callbackURL) { isSuccess, value in
                 print(callbackURL)
