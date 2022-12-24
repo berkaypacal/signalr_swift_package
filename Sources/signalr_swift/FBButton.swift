2b534dade23293eeb30005b30db40638c3dc4d95
berkaypacal <pacalberkay@gmail.com>
2022-12-24 19:30:13 +0300

    update

--- Sources/signalr_swift/FBButton.swift
+++ Sources/signalr_swift/FBButton.swift
@@ -1,6 +1,6 @@
 //
 //  SwiftUIView.swift
-//  
+//
 //
 //  Created by Berkay Paçal on 12/24/22.
 //
@@ -9,10 +9,11 @@ import SwiftUI
 
 @available(iOS 15.0, *)
 public struct FacebookButton: View {
-    
+
     @StateObject public var facebookVM: FacebookViewModel = FacebookViewModel()
-   
- 
+
+    public init() {
+    }
 
     public var body: some View {
         Button("Login with Facebook") {
