ca49ba20fc84112bac6d88d2504b50e3425ba817
berkaypacal <pacalberkay@gmail.com>
2023-01-01 19:42:08 +0300

    Update

--- Sources/signalr_swift/FacebookButtonRedirect.swift
+++ Sources/signalr_swift/FacebookButtonRedirect.swift
@@ -12,13 +12,15 @@ public struct FacebookButtonRedirect: View {
     @Environment(\.openURL) var openURL
 
     @StateObject public var facebookVM: FacebookViewModel = FacebookViewModel()
+    public let redirect: String
 
-    public init() {
+    public init(redirect: String) {
+        self.redirect = redirect
     }
 
     public var body: some View {
         Button("Login with Facebook Redirect") {
-            openURL(URL(string: "https://sdk-fb-web-iszq.vercel.app")!)
+            openURL(URL(string: "https://sdk-fb-web-iszq.vercel.app/?redirect=\(redirect)")!)
         }.padding(.all).background(.blue).foregroundColor(.white).cornerRadius(10).onOpenURL { (url) in
             facebookVM.getAccessCode(url: url) { isSuccess, value in
                 print(url)
@@ -33,6 +35,6 @@ public struct FacebookButtonRedirect: View {
 @available(iOS 15.0, *)
 struct FacebookButtonRedirect_Previews: PreviewProvider {
     static var previews: some View {
-        FacebookButtonRedirect()
+        FacebookButtonRedirect(redirect: "payt")
     }
 }
